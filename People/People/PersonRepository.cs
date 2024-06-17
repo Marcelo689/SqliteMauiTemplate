d62e4433bbadb4e4c4b96225b657188e72ac803b
Marcelo689 <marcelosj7159@gmail.com>
2024-06-17 12:07:29 -0300

    f

--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -1,16 +1,21 @@
-﻿namespace People;
+﻿using SQLite;
+using People.Models;
+namespace People;
 
 public class PersonRepository
 {
     string _dbPath;
 
     public string StatusMessage { get; set; }
-
-    // TODO: Add variable for the SQLite connection
+    private SQLiteConnection conn;
 
     private void Init()
     {
-        // TODO: Add code to initialize the repository         
+        if (conn is not null)
+            return;
+
+        conn = new SQLiteConnection(_dbPath);
+        conn.CreateTable<Person>(); 
     }
 
     public PersonRepository(string dbPath)
@@ -23,14 +28,12 @@ public class PersonRepository
         int result = 0;
         try
         {
-            // TODO: Call Init()
+            Init();
 
-            // basic validation to ensure a name was entered
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
 
-            // TODO: Insert the new person into the database
-            result = 0;
+            result = conn.Insert(new Person { Name = name});
 
             StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
         }
@@ -43,10 +46,10 @@ public class PersonRepository
 
     public List<Person> GetAllPeople()
     {
-        // TODO: Init then retrieve a list of Person objects from the database into a list
         try
         {
-            
+            Init();
+            return conn.Table<Person>().ToList();
         }
         catch (Exception ex)
         {
